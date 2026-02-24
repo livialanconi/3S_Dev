a1d21f810a9763feb838b7ac336fa308729f3731
LÍVIA CAIRES LANÇONI <42113437856@info.local>
2026-02-24 09:08:17 -0300

    .

---
+++ API/Filmes.WebAPI/Program.cs
@@ -0,0 +1,25 @@
+using Filmes.WebAPI.BdContextFilme;
+using Filmes.WebAPI.Interfaces;
+using Filmes.WebAPI.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Adiciona o contexto do banco de dados (exemplo com SQL Server)
+builder.Services.AddDbContext<FilmeContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//Adiciona o repositorio ao container de injeção de dependência
+builder.Services.AddScoped<IGeneroRepository, GeneroRepository>();
+builder.Services.AddScoped<IFilmeRepository, FilmeRepository>();
+
+//Adiciona o serviço de Controllers
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+//Adiciona o mapeamento de Controllers
+app.MapControllers();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
