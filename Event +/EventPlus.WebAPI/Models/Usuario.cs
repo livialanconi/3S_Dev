21d27e9e81bf331a070ca8f65c78c544faf6b956
LÍVIA CAIRES LANÇONI <42113437856@info.local>
2026-03-05 16:38:36 -0300

    .

---
+++ Event +/EventPlus.WebAPI/Models/Usuario.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventPlus.WebAPI.Models;
+
+[Table("Usuario")]
+[Index("Email", Name = "UQ__Usuario__A9D10534A23D6F9F", IsUnique = true)]
+public partial class Usuario
+{
+    [Key]
+    public Guid IdUsuario { get; set; }
+
+    [StringLength(100)]
+    [Unicode(false)]
+    public string Nome { get; set; } = null!;
+
+    [StringLength(256)]
+    [Unicode(false)]
+    public string Email { get; set; } = null!;
+
+    [StringLength(60)]
+    [Unicode(false)]
+    public string Senha { get; set; } = null!;
+
+    public Guid? IdTipoUsuario { get; set; }
+
+    [ForeignKey("IdTipoUsuario")]
+    [InverseProperty("Usuarios")]
+    public virtual TipoUsuario? IdTipoUsuarioNavigation { get; set; }
+
+    [InverseProperty("IdUsuarioNavigation")]
+    public virtual ICollection<Presenca> Presencas { get; set; } = new List<Presenca>();
+}
